b2e72177c2b8e18b1fc5f526f6930e9e08f7c6ac
Devin1824 <devster18@gmail.com>
2022-12-04 20:45:09 -0800

    initial commit

---
+++ MethodOverloading/Methods.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    internal class Methods
+    {
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+
+        public static string Add(int num1, int num2, bool amountMoney)
+        {
+            int sum = num1 + num2;
+            if (amountMoney == true && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (amountMoney == true && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else if (amountMoney == true && sum < 1)
+            {
+                return $"{sum} dollars";
+            }
+            else
+            {
+                return sum.ToString();
+            } 
+            
+        }
+    }
+}
--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Methods.Add(5, 5));
+            Console.WriteLine(Methods.Add(-3.0m, 6.5m));
+            Console.WriteLine(Methods.Add(8, 9, true));
         }
     }
 }
